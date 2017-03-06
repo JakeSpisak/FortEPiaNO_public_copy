b2d098a694b1b3903ba81be58b6d085965f677d7
Stefano Gariazzo <stefano.gariazzo@ific.uv.es>
2017-03-06 11:46:27 +0100

    profile all executables (first arg is exec name)

--- 1profile.sh
+++ 1profile.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
-gprof bin/nuDens.exe | gprof2dot | dot -Tpng -o output.png
+if [ "$1" == "" ]
+then
+	exe=nuDens
+else
+	exe=$1
+fi
+gprof bin/$exe.exe | gprof2dot | dot -Tpng -o output.png
