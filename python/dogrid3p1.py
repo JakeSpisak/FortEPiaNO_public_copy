547865ea508ab2b5858cfac8061c666ee5be28f8
Stefano Gariazzo <steog88@gmail.com>
2019-03-13 18:38:59 +0100

    plots should now work with complete grids; flag to enable using sin^22th as grid parameter

--- python/dogrid3p1.py
+++ python/dogrid3p1.py
@@ -72,6 +72,26 @@ def setParser():
 		help='parameter for the y axis',
 		)
 	parser_plot.add_argument(
+		'--fix_dm41',
+		default=False,
+		help='when requesting a subplot, fix dm41 to the given index (not value!)',
+		)
+	parser_plot.add_argument(
+		'--fix_ssq14',
+		default=0,
+		help='when requesting a subplot, fix ssq14 to the given index (not value!)',
+		)
+	parser_plot.add_argument(
+		'--fix_ssq24',
+		default=False,
+		help='when requesting a subplot, fix ssq24 to the given index (not value!)',
+		)
+	parser_plot.add_argument(
+		'--fix_ssq34',
+		default=0,
+		help='when requesting a subplot, fix ssq34 to the given index (not value!)',
+		)
+	parser_plot.add_argument(
 		'--title',
 		default="",
 		help='title for the plot',
@@ -182,12 +202,21 @@ def fillGrid(args):
 def write_grid_cfg(args):
 	with open("grids/%s/params.cfg"%args.gridname, "w") as _f:
 		for a in ["dm41", "ssq14", "ssq24", "ssq34"]:
-			_f.write("%s: min=%s, max=%s, N=%s\n"%(
-				a,
-				getattr(args, "%s_min"%a),
-				getattr(args, "%s_max"%a),
-				getattr(args, "%s_N"%a),
-				))
+			if "ssq" in a:
+				_f.write("%s: min=%s, max=%s, N=%s, ssq2th=%s\n"%(
+					a,
+					getattr(args, "%s_min"%a),
+					getattr(args, "%s_max"%a),
+					getattr(args, "%s_N"%a),
+					getattr(args, a.replace("ssq", "sinsq2th")),
+					))
+			else:
+				_f.write("%s: min=%s, max=%s, N=%s\n"%(
+					a,
+					getattr(args, "%s_min"%a),
+					getattr(args, "%s_max"%a),
+					getattr(args, "%s_N"%a),
+					))
 
 
 def read_grid_cfg(gridname):
@@ -204,27 +233,52 @@ def read_grid_cfg(gridname):
 					values["%s_N"%a] = int(res.group(3))
 				except (IndexError, TypeError, ValueError):
 					print("cannot read line! %s"%l)
+				else:
+					if values["%s_min"%a] == 0 or values["%s_max"%a] == 0:
+						values["%s_pts"%a] = np.linspace(
+							values["%s_min"%a],
+							values["%s_max"%a],
+							values["%s_N"%a])
+					else:
+						values["%s_pts"%a] = np.logspace(
+							np.log10(values["%s_min"%a]),
+							np.log10(values["%s_max"%a]),
+							values["%s_N"%a])
+			else:
+				res = re.match("%s: min=([0-9\.e\+\-]+), max=([0-9\.e\+\-]+), N=([0-9]+), ssq2th=(True|False)\n"%a, l)
+				if res:
+					try:
+						values["%s_min"%a] = float(res.group(1))
+						values["%s_max"%a] = float(res.group(2))
+						values["%s_N"%a] = int(res.group(3))
+						values[a.replace("ssq", "sinsq2th")] = ast.literal_eval(res.group(4))
+					except (IndexError, TypeError, ValueError):
+						print("cannot read line! %s"%l)
+					else:
+						if values["%s_min"%a] == 0 or values["%s_max"%a] == 0:
+							values["%s_pts"%a] = np.linspace(
+								values["%s_min"%a],
+								values["%s_max"%a],
+								values["%s_N"%a])
+						else:
+							values["%s_pts"%a] = np.logspace(
+								np.log10(values["%s_min"%a]),
+								np.log10(values["%s_max"%a]),
+								values["%s_N"%a])
+	for a in ["ssq14", "ssq24", "ssq34"]:
+		if a.replace("ssq", "sinsq2th") not in values.keys():
+			values[a.replace("ssq", "sinsq2th")] = False
 	return values, fillGrid(Namespace(**values))
 
 
-def contourplotsimple(grid, values, points,
-		px=(2, "ssq24"), py=(0, "dm41"), p3=(1, "ssq14", 0.), p4=(3, "ssq34", 0.), fname=None,
+def contourplot(xv, yv, values, points,
+		fname=None,
 		levels=[0., 0.1, 0.3, 0.5, 0.7, 0.9],
 		title=None,
 		xlab=None, ylab=None,
 		xlim=None, ylim=None,
 		):
-	Nx = values["%s_N"%px[1]]
-	xv = np.logspace(
-		np.log10(values["%s_min"%px[1]]),
-		np.log10(values["%s_max"%px[1]]),
-		Nx)
-	Ny = values["%s_N"%py[1]]
-	yv = np.logspace(
-		np.log10(values["%s_min"%py[1]]),
-		np.log10(values["%s_max"%py[1]]),
-		Ny)
-	zv = points.reshape((Nx, Ny))
+	zv = points.reshape((len(xv), len(yv)))
 	fig = plt.Figure(figsize=(6,4))
 	cf = plt.contourf(xv, yv, zv, levels=levels, cmap=matplotlib.cm.get_cmap('CMRmap'), extend="max")
 	cf.cmap.set_over(cmap(0.95))
@@ -254,13 +308,43 @@ def contourplotsimple(grid, values, points,
 
 def call_plot(args):
 	mixings, fullgrid, fullobjects = call_read(args)
-	points = list(map(lambda x: safegetattr(x, "Neff", 0.)-args.Neff_active, fullobjects))
-	points = np.asarray(points)
-	contourplotsimple(fullgrid, mixings, points,
+	fullpoints = list(map(lambda x: safegetattr(x, "Neff", 0.)-args.Neff_active, fullobjects))
+	fullpoints = np.asarray(fullpoints)
+	cgrid = {}
+	for a in indexes.keys():
+		if args.par_x == a or args.par_y == a:
+			setattr(args, "fix_%s"%a, False)
+		if mixings["%s_N"%a] > 1 and getattr(args, "fix_%s"%a) is False:
+			cgrid["%s_N"%a] = mixings["%s_N"%a]
+			cgrid["%s_min"%a] = mixings["%s_min"%a]
+			cgrid["%s_max"%a] = mixings["%s_max"%a]
+			cgrid["%s_pts"%a] = mixings["%s_pts"%a]
+		else:
+			cgrid["%s_N"%a] = 1
+			cgrid["%s_min"%a] = mixings["%s_pts"%a][int(getattr(args, "fix_%s"%a))]
+			cgrid["%s_max"%a] = mixings["%s_pts"%a][int(getattr(args, "fix_%s"%a))]
+			cgrid["%s_pts"%a] = np.asarray([mixings["%s_pts"%a][int(getattr(args, "fix_%s"%a))]])
+	smallgrid = fillGrid(Namespace(**cgrid))
+	xv = mixings["%s_pts"%args.par_x]
+	yv = mixings["%s_pts"%args.par_y]
+	smallpoints = []
+	for [p1, p2, p3, p4], pt in zip(fullgrid, fullpoints):
+		for n1, n2, n3, n4 in smallgrid:
+			if (p1==n1 and p2==n2 and p3==n3 and p4==n4):
+				smallpoints.append(pt)
+	smallpoints = np.asarray(smallpoints)
+	if "ssq" in args.par_x and mixings[args.par_x.replace("ssq", "sinsq2th")]:
+		xlab = labels[args.par_x].replace(r"\theta", r"2\theta")
+	else:
+		xlab = labels[args.par_x]
+	if "ssq" in args.par_y and mixings[args.par_y.replace("ssq", "sinsq2th")]:
+		ylab = labels[args.par_y].replace(r"\theta", r"2\theta")
+	else:
+		ylab = labels[args.par_y]
+	contourplot(
+		xv, yv, mixings, smallpoints,
 		fname="grids/%s/plots/%s_%s.pdf"%(args.gridname, args.par_x, args.par_y),
-		px=(indexes[args.par_x], args.par_x),
-		py=(indexes[args.par_y], args.par_y),
-		xlab=labels[args.par_x], ylab=labels[args.par_y], title=args.title,
+		xlab=xlab, ylab=ylab, title=args.title,
 		)
 
 
@@ -290,6 +374,12 @@ def call_prepare(args):
 	write_grid_cfg(args)
 	grid = fillGrid(args)
 	for dm41, ssq14, ssq24, ssq34 in grid:
+		if args.sinsq2th14:
+			ssq14 = 0.5*(1.-np.sqrt(1.-ssq14))
+		if args.sinsq2th24:
+			ssq24 = 0.5*(1.-np.sqrt(1.-ssq24))
+		if args.sinsq2th34:
+			ssq34 = 0.5*(1.-np.sqrt(1.-ssq34))
 		prep = [
 			"grids/%s/ini/%s_%s_%s_%s.ini"%(args.gridname, dm41, ssq14, ssq24, ssq34),
 			"grids/%s/OUT/%s_%s_%s_%s/"%(args.gridname, dm41, ssq14, ssq24, ssq34),
@@ -318,11 +408,17 @@ def call_read(args):
 	values, grid = read_grid_cfg(args.gridname)
 	objects = []
 	for dm41, ssq14, ssq24, ssq34 in grid:
-		lab = (r"dm41=%s$ "%dm41
-			+ r"ssq14=%s$ "%ssq14
-			+ r"ssq24=%s$ "%ssq24
-			+ r"ssq34=%s$ "%ssq34
+		lab = (r"dm41=%s "%dm41
+			+ r"ssq14=%s "%ssq14
+			+ r"ssq24=%s "%ssq24
+			+ r"ssq34=%s "%ssq34
 			)
+		if values["sinsq2th14"]:
+			ssq14 = 0.5*(1.-np.sqrt(1.-ssq14))
+		if values["sinsq2th24"]:
+			ssq24 = 0.5*(1.-np.sqrt(1.-ssq24))
+		if values["sinsq2th34"]:
+			ssq34 = 0.5*(1.-np.sqrt(1.-ssq34))
 		folder = "grids/%s/OUT/%s_%s_%s_%s/"%(args.gridname, dm41, ssq14, ssq24, ssq34)
 		obj = None
 		try:
