0f36a34c340212a5bcef1338073e5e20ebc3997a
Stefano Gariazzo <steog88@gmail.com>
2019-04-01 11:13:35 +0200

    note on valgrind use for profiling

--- 1profile.sh
+++ 1profile.sh
@@ -6,3 +6,7 @@ else
 	exe=$1
 fi
 gprof bin/$exe.exe | gprof2dot | dot -Tpng -o output.png
+
+#other way:
+#valgrind --tool=callgrind bin/nuDens.exe ...
+#gprof2dot -f callgrind callgrind.out.X | dot -Tpng -o output.png
