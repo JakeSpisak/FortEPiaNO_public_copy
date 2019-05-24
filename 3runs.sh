b1f4541b5497ed732d97fd07d64f3c3f180ff60b
Stefano Gariazzo <steog88@gmail.com>
2019-05-24 10:50:41 +0200

    removed private scripts/files from master branch

--- 3runs.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-make BUILD_DIR=buildpar/ EXECNAME=parallel.exe
-
-export OMP_NUM_THREADS=8
-ini=ini/debug.ini
-rm -r `grep outputFolder $ini |sed "s#outputFolder = ##g"`/*
-sleep 1s
-./bin/parallel.exe $ini
--- 8plot_single.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-python -c 'import matplotlib;matplotlib.use("Qt5Agg");import matplotlib.pyplot as plt;from fortepianoOutput import FortEPiaNORun;run = FortEPiaNORun("'$1'", nnu='$2', plots='$3')'
