45f2fdf4ce8e2b2290f1174acd94b019ad44b9fe
Stefano Gariazzo <steog88@gmail.com>
2020-05-05 22:55:33 +0200

    bug fixed

--- sources/equations.f90
+++ sources/equations.f90
@@ -155,7 +155,11 @@ module fpEquations
 			end do
 			numw = g12(1) + elContr0(1)
 			denw = PISQ/7.5d0 + g12(2) + elContr0(2)
+#ifdef NO_MUONS
+			do j=1, fermions_number
+#else
 			do j=2, fermions_number
+#endif
 				fContr = fermions(j)%dzodx_terms(xoz)
 				numw = numw + fContr(1)
 				denw = denw + fContr(2)
@@ -249,7 +253,11 @@ module fpEquations
 		end do
 		numw = g12(1) + elContr0(1)
 		denw = PISQ/7.5d0 + g12(2) + elContr0(2)
+#ifdef NO_MUONS
+		do j=1, fermions_number
+#else
 		do j=2, fermions_number
+#endif
 			fContr = fermions(j)%dzodx_terms(xoz)
 			numw = numw + fContr(1)
 			denw = denw + fContr(2)
