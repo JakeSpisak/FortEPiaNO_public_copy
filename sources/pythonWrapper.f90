f07965c7e6ab1b58c69452dcd4bb6e6d12f845fe
Stefano Gariazzo <steog88@gmail.com>
2021-05-11 01:04:06 +0200

    very simple, but working, wrapper attempt

--- sources/pythonWrapper.f90
+++ sources/pythonWrapper.f90
@@ -6,8 +6,22 @@ module fortepianoWrapper
 
     contains
 
+    !functions for const
     subroutine storeNuDensMat(vec)
-        real(kind=1.d0), dimension(:), intent(in) :: vec
-        vec_2_densMat(vec)
+        real(kind=kind(0.d0)), dimension(:), intent(in) :: vec
+        call vec_2_densMat(vec)
     end subroutine storeNuDensMat
+
+    !functions for cosmology
+    function w_photonNumberDensity(z) result(r)
+        real(kind=kind(0.d0)), intent(in) :: z
+        real(kind=kind(0.d0)) :: r
+        r=photonNumberDensity(z)
+    end function w_photonNumberDensity
+
+    function w_photonDensity(z) result(r)
+        real(kind=kind(0.d0)), intent(in) :: z
+        real(kind=kind(0.d0)) :: r
+        r=photonDensity(z)
+    end function w_photonDensity
 end module fortepianoWrapper
---
+++ sources/testwrapper.py
@@ -0,0 +1,4 @@
+from fortepianoWrapper import fortepianowrapper as fp
+import numpy as np
+
+assert fp.w_photondensity(1.2)==np.pi**2/15 * 1.2**4
